6d4a5caa51b78f2102f0fcdc6b71a8f6794d8e38
Mario <marioseliasioannou@gmail.com>
2022-03-15 01:30:45 +0200

    First Commit

---
+++ app/src/main/java/com/marioioannou/currencycalc/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.marioioannou.currencycalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.marioioannou.currencycalc.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnDigit0.setOnClickListener {
+            Toast.makeText(this,"0",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
